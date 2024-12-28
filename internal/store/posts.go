71d59ad698092c661cf7617589dc1fec842ae30d
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-12-28 23:45:33 +0330

    POST: new endpoint for getting a post was implemented.

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/lib/pq"
 )
@@ -21,12 +22,12 @@ type Post struct {
 	UpdatedAt string   `json:"updated_at"`
 }
 
-func (s *PostStore) Create(ctx context.Context, post *Post) error {
+func (ps *PostStore) Create(ctx context.Context, post *Post) error {
 	query := `
 	  INSERT INTO posts (title, user_id, content, tags)
 	  VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
 	`
-	err := s.db.QueryRowContext(
+	err := ps.db.QueryRowContext(
 		ctx,
 		query,
 		post.Title,
@@ -44,4 +45,34 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	}
 
 	return nil
+}
+
+func (ps *PostStore) GetByID(ctx context.Context, postID int64) (*Post, error) {
+	query := `
+		SELECT id, user_id, title, content, created_at, updated_at, tags
+		FROM posts WHERE id = $1 LIMIT 1;
+	`
+
+	var post Post
+
+	err := ps.db.QueryRowContext(ctx, query, postID).Scan(
+		&post.ID,
+		&post.UserID,
+		&post.Title,
+		&post.Content,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+		pq.Array(&post.Tags),
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &post, nil
 }
